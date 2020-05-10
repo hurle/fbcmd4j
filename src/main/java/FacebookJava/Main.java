c0806d911fea5c72603a85375b2e28455dadbc7a
Ulises Castañeda <francisco.ulises.c@gamil.com>
2020-05-10 01:20:23 -0500

    Creacion de metodo para publicar links

---
+++ src/main/java/FacebookJava/Main.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FacebookJava;
+
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.Post;
+import facebook4j.ResponseList;
+import facebook4j.auth.AccessToken;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+import java.util.Scanner;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Ulises Castañeda
+ */
+public class Main {
+    private static final String CONFIG_FILE = "fbcmd4j.properties";
+    private static Scanner scan = new Scanner(System.in);
+    private static Properties props = new Properties();
+    private static Facebook fb = null;
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) throws IOException, FacebookException 
+    {
+        Main main = new Main();
+        main.SetProperties();
+        main.SetFacebook();
+        
+        
+         while(true) {
+            int option;
+            System.out.println(":---------Ingresa la opcion que necesitas:---------\n");
+            logger.info("App Iniciada");
+            System.out.println("NewsFeed :   --------- 1\n");
+            System.out.println("Wall:        --------- 2\n");
+            System.out.println("Estado:      --------- 3\n");
+            System.out.println("Link:        --------- 4\n");
+            System.out.println("Salir:       --------- 5\n");
+            
+            option = scan.nextInt();
+            scan.nextLine();
+            switch (option) 
+            {
+                case 1:
+                    try
+                    {
+                        System.out.println("Obteniendo NewsFeed");
+                        ResponseList<Post> newsFeed = fb.getFeed("ucastaneda21");
+                        for (Post p : newsFeed)
+                        {
+                            Tools.printPost(p);
+                        }
+                        logger.info("NewsFeed cargado correctamente.");
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.println("Error en NewsFeed:  " + ex.getMessage());
+                    }
+                    break;
+                case 2:
+                    try
+                    {
+                        System.out.println("Mostrando Wall...");
+                        ResponseList<Post> wall = fb.getPosts();
+                        for (Post p : wall) {
+                            Tools.printPost(p);
+                        }
+                        logger.info("Wall Cargado correctamente.");
+                        }
+                    catch(Exception ex)
+                    {
+                        logger.info("Error en Wall: " + ex.getMessage());
+                    }
+                    break;
+                case 3:
+                    try
+                    {
+                        System.out.println("Ingrese el texto a publicar.");
+                        String status = scan.nextLine();
+                        fb.postStatusMessage(status);
+                        System.out.println("Se publico el estado correctamente.");
+                    }
+                    catch(Exception ex)
+                    {
+                        logger.info("Error al hacer publicacion: " + ex.getMessage());
+                    }
+                    break;
+                case 4:
+                    try 
+                    {
+                        System.out.println("Ingresa el link: ");
+                        String link = scan.nextLine();
+			fb.postLink(new URL(link));
+                        
+                        System.out.println("Se publico el link correctamente.");
+                    }
+                    catch(Exception ex)
+                    {
+                        logger.info("Error al intentar publicar el link: " + ex.getMessage());
+                    }
+                    break;
+                case 5:
+                    System.out.println("-----Saliendo del progrma-----");
+                    System.exit(0);
+            }
+        }
+    }
+    
+    
+    void SetProperties() throws IOException
+    {
+        try
+        {
+            InputStream inStream = getClass().getClassLoader().getResourceAsStream(CONFIG_FILE);
+            props.load(inStream);
+            logger.info("Propiedades Correctas.");
+        }
+        catch(Exception ex)
+        {
+            logger.info("Error en las propiedades:  " + ex.getMessage());
+        }
+    }
+    
+    void SetFacebook()
+    {
+        try
+        {
+            fb = new FacebookFactory().getInstance();
+            fb.setOAuthAppId(props.getProperty("oauth.appId"), props.getProperty("oauth.appSecret"));
+            fb.setOAuthPermissions(props.getProperty("oauth.permissions"));
+            if(props.getProperty("oauth.accessToken") != null)
+            {
+                fb.setOAuthAccessToken(new AccessToken(props.getProperty("oauth.accessToken"), null));
+                logger.info("Facebook properties correctas");
+            }
+        }
+        catch(Exception ex)
+        {
+            logger.info("Error al inicializar Facebook:  " + ex.getMessage());
+        }
+        
+    }
+}
---
+++ src/main/java/FacebookJava/Tools.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FacebookJava;
+
+import facebook4j.Facebook;
+import facebook4j.FacebookFactory;
+import facebook4j.Post;
+import facebook4j.auth.AccessToken;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/**
+ *
+ * @author Ulises Castañeda
+ */
+public class Tools {
+    private InputStream inputStream = null;
+    
+    public InputStream getStream(String fileName)
+    {
+        InputStream stream = getClass().getClassLoader().getResourceAsStream(fileName);
+        return stream;
+    }
+    
+    
+    public static Facebook InstanceFacebook(Properties props) 
+    {
+        Facebook fb = new FacebookFactory().getInstance();
+        try
+        {
+            fb.setOAuthAppId(props.getProperty("oauth.appId"), props.getProperty("oauth.appSecret"));
+            fb.setOAuthPermissions(props.getProperty("oauth.permissions"));
+            fb.setOAuthAccessToken(new AccessToken(props.getProperty("oauth.accessToken"), null));
+            return fb;
+        }
+        catch(Exception ex )
+        {
+            return fb;
+        }
+    }
+    
+    public static void printPost(Post p) 
+    {
+        if(p.getStory() != null)
+            System.out.println("Story: " + p.getStory());
+        if(p.getMessage() != null)
+            System.out.println("Mensaje: " + p.getMessage());
+        System.out.println("--------------------------------");
+    }
+    
+  
+}
